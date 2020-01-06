6e66d927138e9ce4bd10d62458836e94c8cee486
Anna Mai <annam0403@gmail.com>
2020-01-06 12:56:53 -0500

    Update README

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div>
+        Hm... I think you could add some components to make this view more
+        interesting
+      </div>
+      {/* Add a home bar */}
+      {/* Add your feed */}
+    </div>
+  );
+};
