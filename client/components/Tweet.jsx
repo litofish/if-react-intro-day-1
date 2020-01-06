862627b773dfd39a7138d96b641ff157a42c8ac0
Anna Mai <annam0403@gmail.com>
2020-01-06 13:18:20 -0500

    more boilerplate styles, some boilerplate jsx

--- client/components/Tweet.jsx
+++ client/components/Tweet.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Tweet = props => {
   return (
-    <div className="tweet-card">
-      <div>{/* profile photo */}</div>
-      <div>{/* other content */}</div>
+    <div className="tweet-card flex-row">
+      <div className="profile-photo">{/* profile photo */}</div>
+      <div className="content">
+        {/*
+            other content. hint:
+            - div for user info
+            - div for content
+            - div for displaying likes. use and style color <i class="fas fa-heart"></i>
+          */}
+      </div>
     </div>
   );
 };
