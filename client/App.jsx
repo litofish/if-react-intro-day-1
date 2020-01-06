09981da8203d213f35f3eafe26cd0ebc69c728a4
Anna Mai <annam0403@gmail.com>
2020-01-06 16:00:09 -0500

    Toggle Color demo code

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+import ToggleColor from './ToggleColor';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      bgColor: 'default'
+      bgColor: 'red',
+      options: ['default', 'red', 'blue', 'green']
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(color) {
+    this.setState({
+      bgColor: color
+    });
   }
 
   render() {
     return (
-      <div className="container default">
-        <div className="square" />
+      <div className="container">
+        <div className={`square ${this.state.bgColor}`} />
         <div className="colors">
-          <div className="toggle default" />
-          <div className="toggle red" />
-          <div className="toggle blue" />
-          <div className="toggle green" />
+          {this.state.options.map(option => (
+            <ToggleColor
+              color={option}
+              handleClick={() => this.handleClick(option)}
+              key={option}
+            />
+          ))}
         </div>
       </div>
     );
