3d521878bd0b84195779a677fe34ad6dfe9ab8de
litofish <luna@itofisher.com>
2020-01-06 16:03:19 -0500

    color example code

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
+import ToggleColor from './ToggleColor'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      bgColor: 'default'
+      bgColor: 'red',
+      options: ['default', 'red', 'blue', 'green']
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(color) {
+      this.setState({
+         bgColor: color
+      });
   }
 
   render() {
     return (
-      <div className="container default">
-        <div className="square" />
+        // {} says we need to evaluate the name, so can add in parameters
+      <div className="container">
+        <div className={`square ${this.state.bgColor}`} />
         <div className="colors">
-          <div className="toggle default" />
-          <div className="toggle red" />
-          <div className="toggle blue" />
-          <div className="toggle green" />
+          {
+              this.state.options.map(option => (
+                  <ToggleColor color={option}
+                  handleClick={() => this.handleClick(option)}
+                  // needs a key when using map, identifier for react
+                  key={option}
+                  />
+              ))
+          }
         </div>
       </div>
     );
---
+++ client/ToggleColor.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ToggleColor = props => {
+    const color = props.color;
+    const handleClick = props.handleClick;
+
+    return (
+        <div
+        className={`toggle ${color}`}
+        onClick={handleClick}
+        />
+    );
+}
+
+export default ToggleColor;
